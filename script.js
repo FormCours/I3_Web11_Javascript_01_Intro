0af0eec88c681cde2c1a7d5769f93babbc9c71d2
Aurélien <aurelien.strimelle@bstorm.be>
2022-05-31 16:44:23 +0200

    Demo Variable

--- script.js
+++ script.js
@@ -96,3 +96,96 @@ btn.addEventListener('click', () => {
     // Modifier le contenu de la balise "input"
     baliseInput.value = '';
 });
+
+/**************************************************************************/
+
+// Les variables
+// *************
+
+/* Elle se compose :
+    - Nom
+    - Type
+    - Contenu (Chiffre, du texte, booléen...)
+    - Adresse mémoire [Géré par le systeme]
+*/
+
+/* Le JS est un langage a type dynamique
+   Le systeme va définir le typage adapté pour le contenu
+
+   => Bonne pratique : Evité de changer le typage d'un variable
+*/
+
+/* Nature de variable possible
+    - let   : Variable locale
+    - const : Constante locale
+    - var   : Variable globale    /!\ A evité :o
+*/
+
+/* Valeur prédéfini
+    - undefined : Valeur non défini (Pas initialisé)
+    - null      : Value initialisé (objet) mais sans valeur
+*/
+
+/* Conventions de nommage
+    - Les bonnes pratiques pour nommé vos variables 
+    - Format « camelCase »
+    - Les caracteres autorisé : a-z 0-9 _ $
+    - Le premier caractere ne peut pas etre un chiffre
+    - Des mots clefs sont réservé pour le systeme (if, switch, class, ...)
+*/
+
+
+
+
+// Les comparaisons
+// ****************
+
+// - Double egale : Verification du contenu (sans typage)  
+  0 == "0"  // true
+  0 == []   // true
+"0" == []   // false    => Pourquoi :o
+
+// Le systeme utilise des conversions pour résoudre l'egalité
+//  => Mauvaise idée
+
+// - Triple egale : Verification du typage et ensuite du contenu
+  0 === "0"  // false
+  0 === []   // false
+"0" === []   // false
+
+
+/**************************************************************************/
+
+// Les chaine de caracteres (String)
+// *********************************
+
+// => Une variable qui permet de stocker un ensemble de caractere
+
+// Déclaration
+// - Une chaine de caractere sont toujours entre simple ou double quote
+const msgInfo = 'Ceci est une chaine de caractere'
+console.log(msgInfo);
+
+// - Le backslash est un caractere d'echapement
+const animal = 'C\'est "l\'oiseau" qui a volé le formage !';
+const backshash = 'Le symbole backslash est \\';
+console.log(animal);
+console.log(backshash);
+
+// - Fusionner des chaines de caraceteres 
+const firstname = 'Zaza';
+const lastname = 'Vanderquack';
+const age = 13;
+
+// L'operateur « + » permet de concaténé des chaine de caractere.
+const phrase1 = 'Je suis ' + firstname + ' ' + lastname + ' et j\'ai ' + age + ' ans.';
+
+// La méthode « concat »
+const phrase2 = 'Je suis'.concat(firstname).concat(' ').concat(lastname)
+                         .concat(' et j\'ai ').concat(age).concat(' ans.');
+
+const phase3 = ''.concat('Je suis', firstname, ' ', lastname, ' et j\'ai ', age, ' ans.');
+
+// Le Template String (Back-tick)
+const phase4 = `Je suis ${firstname} ${lastname} et j'ai ${age} ans.`;
+// Note : Les `` doivent être utiliser s'il y a de la concaténation !
